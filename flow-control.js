9c023db849c905793e6507821a57563d0d7a40b0
hilarious-programmer-2558 <combatdstar@gmail.com>
2019-08-17 05:58:35 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -46,7 +46,7 @@ function switchAge(age) {
     case 19:
     return 'You are a teenager';
     case age>=20:
-    return 'You are a kid';
+    return 'You are a grownup';
     default:
     return 'You have an age';
   }
